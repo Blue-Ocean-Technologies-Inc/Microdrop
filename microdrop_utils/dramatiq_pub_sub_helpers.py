5fa398a167b6892045e3d1ba0fb8a815cfe45ec2
Alexandros Sklavounos <alex_sklavounos@yahoo.gr>
2025-05-23 21:14:11 -0400

    remove print statement from helper

--- microdrop_utils/dramatiq_pub_sub_helpers.py
+++ microdrop_utils/dramatiq_pub_sub_helpers.py
@@ -17,7 +17,7 @@ def publish_message(message, topic, actor_to_send="message_router_actor", queue_
     Publish a message to a given actor with a certain topic
     """
     logger.debug(f"Publishing message: {message} to actor: {actor_to_send} on topic: {topic}")
-    # print(f"Publishing message: {message} to actor: {actor_to_send} on topic: {topic}")
+
     broker = dramatiq.get_broker()
 
     if message_options is None:
