895ac22799099ee714f98cd772349f237ebba92e
Alexandros Sklavounos <alex_sklavounos@yahoo.gr>
2025-05-29 02:33:16 -0400

    Running a child window from Task

--- device_viewer/task.py
+++ device_viewer/task.py
@@ -6,7 +6,7 @@ import dramatiq
 # Enthought library imports.
 from pyface.tasks.action.api import SMenu, SMenuBar, TaskToggleGroup, TaskAction
 from pyface.tasks.api import Task, TaskLayout, Tabbed
-from pyface.api import FileDialog, OK
+from pyface.api import FileDialog, OK, GUI
 from traits.api import Instance, Str, provides
 
 from microdrop_utils.i_dramatiq_controller_base import IDramatiqControllerBase
@@ -23,30 +23,29 @@ from microdrop_utils._logger import get_logger
 from microdrop_utils.dramatiq_controller_base import generate_class_method_dramatiq_listener_actor, \
     basic_listener_actor_routine
 
+from dropbot_tools_menu.self_test_dialogs import WaitForTestDialogAction, ResultsDialogAction
+
 logger = get_logger(__name__, level="DEBUG")
 DEFAULT_SVG_FILE = f"{os.path.dirname(__file__)}{os.sep}2x3device.svg"
 
 listener_name = f"{PKG}_listener"
 
-from dropbot_tools_menu.menus import ALL_TESTS
-import threading
-from dropbot_tools_menu.menus import parse_test_voltage_html_report, parse_on_board_feedback_calibration_html_report, parse_scan_test_board_html_report
-from dropbot_tools_menu.self_test_dialogs import ResultsDialog, SelfTestIntroDialog, ScanTestBoardResultsDialog
-# from PySide6 import Qt
 
 @provides(IDramatiqControllerBase)
 class DeviceViewerTask(Task):
     ##########################################################
     # 'IDramatiqControllerBase' interface.
     ##########################################################
-    # for debugging purposes
-    last_test_mode = Str("individual")  # or 'all'
-    # TODO: Child window should be an instance of DeviceViewerTask
-    # i.e., child_window = Instance(DeviceViewerTask)
+
+    # Child window should be an instance of Dialog Action
+    wait_for_test_dialog = Instance(WaitForTestDialogAction)
     dramatiq_listener_actor = Instance(dramatiq.Actor)
 
     listener_name = f"{PKG}_listener"
-
+    
+    #### 'DeviceViewerTask' interface #########################
+    electrodes_model = Instance(Electrodes)
+    
     def listener_actor_routine(self, message, topic):
         return basic_listener_actor_routine(self, message, topic)
 
@@ -106,9 +105,6 @@ class DeviceViewerTask(Task):
 
         self.electrodes_model = _electrodes
 
-    #### 'DeviceViewerTask' interface ##########################################
-
-    electrodes_model = Instance(Electrodes)
 
     ###########################################################################
     # Protected interface.
@@ -187,154 +183,38 @@ class DeviceViewerTask(Task):
         current_test_id = message.get('current_test_id')
         total_tests = message.get('total_tests')
         report_path = message.get('report_path')
-        # active_state = message.get('active_state')
-        # current_message = message.get('current_message')
-        # done_test_number = message.get('done_test_number')
-        # report_path = message.get('report_path')
-        # test_name = message.get('test_name')x
         
-       
-        if current_test_id == 0:
-             # TODO: start child window here when current_test_id == 0
-            # i.e., self.make_child_window()
-            pass
-        
-        current_test_id += 1
-        
-        logger.info(f"Handler called. test_name = {current_test_name}, Running test = {current_test_id} / {total_tests}")
-        
-        # print(current_message, threading.current_thread().name)
-        
-        # if self.progress_bar is not None:
-        if not active_state:
-            # TODO: Progress bar should be part of the child window and the message should be an attribute of the child window
-            # self.progress_bar.current_message = "Done running all tests. Generating report...\n"
-            pass
-        else:
-            # TODO: Progress bar should be part of the child window and the message should be an attribute of the child window
-            # self.progress_bar.current_message = f"Processing: {current_test_name}\n"
-            pass
-        
-        # sort sleep to update the progress bar
-        if current_test_id is not None:
-            percentage = int((current_test_id / total_tests) * 100)
-            logger.debug(f"Progress: {percentage}")
-            # TODO: Progress bar should be part of the child window and the message should be an attribute of the child window
-            # self.progress_bar.progress = percentage
-
-        #if not active_state and report_path :
-            # TODO: Close child window here when all tests are done
-            # i.e., self.close_child_window()
-        #elif not active_state and report_path is None:
-            # TODO: Close child window here when all tests are done and create a new one with the report
-            # i.e., self.close_child_window()
+        def show_dialog():
+            self.wait_for_test_dialog = WaitForTestDialogAction()
+            if total_tests == 1:
+                test_name = current_test_name.replace("_", " ").capitalize()
+                self.wait_for_test_dialog.perform(self, 
+                                                  test_name=test_name)
+            else:
+                test_name = 'All Tests'
+                self.wait_for_test_dialog.perform(self, 
+                                                  test_name=test_name, 
+                                                  mode="progress_bar")
             
-            # open_html_in_browser(report_path)
-            # close progress bar
-            # if hasattr(self, "progress_bar_ui") and self.progress_bar_ui is not None:
-            #     self.progress_bar_ui.dispose()
-            #     self.progress_bar_ui = None
-            # self.progress_bar = None
+        if current_test_id == 0 and current_test_name is not None:
+            # Start child window here when current_test_id == 0
+            # Force the dialog to be shown in the next event loop iteration
+            GUI.invoke_later(show_dialog)
+            
+        logger.info(f"Handler called. test_name = {current_test_name}, Running test = {current_test_id} / {total_tests}")
 
-            # show report on html or on results dialog depending on last_test_mode
-            # Handle results for each individual test by name
-            # if getattr(self, 'last_test_mode', 'individual') == "individual":
-            #     if test_name == 'test_voltage':
-            #         parsed_data = parse_test_voltage_html_report(report_path)
-            #         dialog = ResultsDialog(
-            #             parent=None,
-            #             table_data=parsed_data.get('table'),
-            #             rms_error=parsed_data.get('rms_error'),
-            #             plot_data=parsed_data.get('plot_data')
-            #         )
-            #     elif test_name == 'test_on_board_feedback_calibration':
-            #         parsed_data = parse_on_board_feedback_calibration_html_report(report_path)
-            #         dialog = ResultsDialog(
-            #             parent=None,
-            #             table_data=parsed_data.get('table'),
-            #             rms_error=None,
-            #             plot_data=parsed_data.get('plot_data')
-            #         )
-            #     elif test_name == 'test_channels':
-            #         parsed_data = parse_scan_test_board_html_report(report_path)
-            #         dialog = ScanTestBoardResultsDialog(
-            #             parent=None,
-            #             description_text=parsed_data.get('description_text', ''),
-            #             images=parsed_data.get('images', [])
-            #         )                       
-            #     else:
-            #         # add other individual tests here
-            #         return
-            #     dialog.exec_()
-            # elif getattr(self, 'last_test_mode', 'all') == "all":
-            #     open_html_in_browser(report_path)
+        # Update the progress bar if in progress mode.
+        if total_tests > 1:
+            if not active_state:
+                percentage = 100
+            else:
+                percentage = int((current_test_id / total_tests) * 100)
+                
+            logger.debug(f"Progress: {percentage}")
+            GUI.invoke_later(self.wait_for_test_dialog.set_progress, 
+                             percentage, 
+                             current_test_name)
 
-
-# new imports (move to top once complete)
-from traits.api import HasTraits, Str, Int
-from traitsui.editors.progress_editor import ProgressEditor
-from traitsui.api import View, HGroup, UItem
-
-class ProgressBar(HasTraits):
-    """A TraitsUI application with a progress bar."""
-
-    current_message = Str()
-    progress = Int(0)
-    num_tasks = Int(1)
-
-    def _progress_default(self):
-        return 0
-
-    def _num_tasks_default(self):
-        return 1
-
-    traits_view = View(
-        HGroup(
-            UItem(
-                "progress",
-                editor=ProgressEditor(
-                    message_name="current_message", 
-                    min_name="progress", 
-                    max_name="num_tasks",
-                ),
-            ),
-        ),
-        title="Running Dropbot On-board Self-tests...",
-        resizable=True,
-        width=400,
-        height=100
-    )
-
-
-class MakeChildWindow(HasTraits):
-    def __init__(self, parent=None):
-        self.parent = parent
-
-    def make_child_window(self):
-        pass
-    
-    def create_child_window(self):
-        # set test mode separately
-        if self.num_tests == len(ALL_TESTS):
-            task.last_test_mode = "all"         
-        else:
-            task.last_test_mode = "individual"
-
-        app = QtWidgets.QApplication.instance()
-        parent_window = app.topLevelWidgets()[0] if app and app.topLevelWidgets() else None
-        intro_dialog = SelfTestIntroDialog(parent=parent_window)
-        result = intro_dialog.exec_()
-        if result != QtWidgets.QDialog.Accepted:
-            return
-        
-        # progress bar
-        if not hasattr(task, "progress_bar") or task.progress_bar is None:
-            task.progress_bar = ProgressBar(num_tasks=self.num_tests)
-            # task.progress_bar_ui = self.progress_bar.edit_traits()
-        else:
-            task.progress_bar.num_tasks = self.num_tests
-        task.progress_bar.progress = 0
-        task.progress_bar.current_message = "Starting...\n"
-
-        # show progress bar
-        task.progress_bar_ui = task.progress_bar.edit_traits()        
+        # Close the dialog when the test is done
+        if not active_state:
+            GUI.invoke_later(self.wait_for_test_dialog.close)
