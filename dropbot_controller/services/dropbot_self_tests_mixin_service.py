2864bdbd3a561e595010d93b0670aa030e181fd2
Alexandros Sklavounos <alex_sklavounos@yahoo.gr>
2025-05-29 02:33:31 -0400

    Running a child window standalone

--- dropbot_controller/services/dropbot_self_tests_mixin_service.py
+++ dropbot_controller/services/dropbot_self_tests_mixin_service.py
@@ -9,17 +9,24 @@ from dropbot.hardware_test import (ALL_TESTS, system_info, test_system_metrics,
                                    test_on_board_feedback_calibration,
                                    test_channels)
 
-from dropbot.self_test import generate_report
-from traits.api import provides, HasTraits
+from dropbot.self_test import (generate_report, plot_test_voltage_results, 
+                               plot_test_on_board_feedback_calibration_results, 
+                               plot_test_channels_results)
+from traits.api import provides, HasTraits, Str, Instance
 
 from microdrop_utils.file_handler import open_html_in_browser
 from microdrop_utils._logger import get_logger
 from microdrop_utils.dramatiq_pub_sub_helpers import publish_message
 from ..consts import SELF_TESTS_PROGRESS
 
-logger = get_logger(__name__, level="DEBUG")
-
 from ..interfaces.i_dropbot_control_mixin_service import IDropbotControlMixinService
+
+
+from pyface.api import GUI
+
+from dropbot_tools_menu.self_test_dialogs import ResultsDialogAction
+
+logger = get_logger(__name__, level="DEBUG")
 
 
 def get_timestamped_results_path(test_name: str, path: [str, Path]) -> Path:
@@ -98,9 +105,10 @@ class DropbotSelfTestsMixinService(HasTraits):
     A mixin Class that adds methods to set states for a dropbot connection and get some dropbot information.
     """
 
-    id = "dropbot_self_tests_mixin_service"
-    name = 'Dropbot Self Tests Mixin'
-
+    id = Str("dropbot_self_tests_mixin_service")
+    name = Str('Dropbot Self Tests Mixin')
+    results_dialog = Instance(ResultsDialogAction)
+    
     ######################################## private methods ##############################################
 
     @staticmethod
@@ -134,8 +142,29 @@ class DropbotSelfTestsMixinService(HasTraits):
             if report_path is not None:
                 open_html_in_browser(report_path)
             else:
-                # TODO: pull up the report in a window 
-                pass
+                plot_data = None
+                if test_name == "test_voltage":
+                    plot_data = plot_test_voltage_results(result[test_name], 
+                                                          return_fig=True)
+                elif test_name == "test_on_board_feedback_calibration":
+                    plot_data = plot_test_on_board_feedback_calibration_results(result[test_name], 
+                                                                                return_fig=True)
+                elif test_name == "test_channels":
+                    plot_data = plot_test_channels_results(result[test_name], 
+                                                           return_fig=True)
+                            
+                if plot_data is not None:
+                    # Pull up the report in a window 
+                    def show_results_dialog():
+                        self.results_dialog = ResultsDialogAction()
+                        test_name_display = test_name.replace("_", " ").capitalize() + " Results"
+                        self.results_dialog.perform(title=test_name_display, 
+                                                    plot_data=plot_data)
+                    GUI.invoke_later(show_results_dialog)
+                else:
+                    # TODO: show the result of short test
+                    pass
+
             # do whatever else is defined in func
             func(self, report_generation_directory)
 
