d7e791e6cbe7b9ae06911d66a60168c90ac7db1c
numberisnan <github@farazkaleemmalik.cyou>
2025-07-03 09:54:55 -0400

    MIC-57 Bugfix in ListChangeCommand and time-based merging

--- device_viewer/utils/commands.py
+++ device_viewer/utils/commands.py
@@ -1,6 +1,10 @@
-from traits.api import Instance, Str, HasTraits, Tuple
+from traits.api import Instance, Str, Float
 from pyface.undo.abstract_command import AbstractCommand
 from traits.observation.events import ListChangeEvent, TraitChangeEvent
+import time
+from microdrop_utils._logger import get_logger
+
+logger = get_logger(__name__)
 
 class TraitChangeCommand(AbstractCommand):
 
@@ -9,9 +13,10 @@ class TraitChangeCommand(AbstractCommand):
     event = Instance(TraitChangeEvent)
 
     def do(self):
-        pass
+        self.timestamp = time.time()
 
     def undo(self):
+        logger.info(f"Undoing set {self.event.name} on {self.event.object} from {self.event.old} to {self.event.new}")
         setattr(self.event.object, self.event.name, self.event.old)
 
 class ListChangeCommand(AbstractCommand):
@@ -20,11 +25,24 @@ class ListChangeCommand(AbstractCommand):
 
     event = Instance(ListChangeEvent)
 
+    timestamp = Float()
+
     def do(self):
-        pass
+        self.timestamp = time.time()
+
+    def merge(self, other):
+        merge_timestamp = time.time()
+        
+        if isinstance(other, ListChangeCommand) and other.event.object == self.event.object and merge_timestamp - self.timestamp <= 1.5:
+            if len(self.event.removed) == 0 and len(other.event.removed) == 0: # Only added
+                self.event.added.extend(other.event.added)
+                return True
+        
+        return False
 
     def undo(self):
+        logger.info(f"Undoing list mod {self.event.object}, added {self.event.added}, removed {self.event.removed} at {self.event.index}")
         for _ in self.event.added:
             self.event.object.pop(self.event.index)
-        for i, item in enumerate(self.event.removed):
+        for item in reversed(self.event.removed):
             self.event.object.insert(self.event.index, item)
