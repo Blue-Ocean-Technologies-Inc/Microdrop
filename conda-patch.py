d2263a13b31e54d44a58eb107c56a8273ab37b04
numberisnan <github@farazkaleemmalik.cyou>
2025-08-19 09:28:50 -0400

    Automate patching of pyface with fixed qt6 import

---
+++ conda-patch.py
@@ -0,0 +1,12 @@
+import shutil
+from pathlib import Path
+
+# Patches should be idempotent (i.e., can be applied multiple times without changing the result beyond the initial application).
+# This allows re-running the patch script without worrying about whether a patch has already been applied.
+
+# Patch pyface/toolkit.py to use Qt toolkit
+# This patch is applied to make dynamic import explicit for PyInstaller.
+# See https://github.com/enthought/pyface/issues/350#issuecomment-632545893
+import pyface.toolkit as toolkit
+shutil.copy2("patches/toolkit.py", str(Path(toolkit.__file__).parent / "toolkit.py"))
+
