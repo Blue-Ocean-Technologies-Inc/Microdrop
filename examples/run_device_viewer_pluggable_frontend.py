a97002b0abee903e01f35dc38071f1e758ceb168
Vigneshwar Rajesh <67299496+Vigne-hub@users.noreply.github.com>
2025-09-10 13:23:36 -0400

    - run script for frontend needs redis server to run. - Updated to not call run function with args.

--- examples/run_device_viewer_pluggable_frontend.py
+++ examples/run_device_viewer_pluggable_frontend.py
@@ -4,18 +4,18 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from examples.run_device_viewer_pluggable import main as run_device_viewer_pluggable
-from examples.plugin_consts import (REQUIRED_PLUGINS, FRONTEND_PLUGINS, 
-                                    FRONTEND_CONTEXT, REQUIRED_CONTEXT, 
+from examples.plugin_consts import (REQUIRED_PLUGINS, FRONTEND_PLUGINS,
+                                    FRONTEND_CONTEXT, REQUIRED_CONTEXT,
                                     FRONTEND_APPLICATION)
 
 
-def main(args):
+def main():
     """Run only the frontend plugins."""
     plugins = REQUIRED_PLUGINS + FRONTEND_PLUGINS
     contexts = FRONTEND_CONTEXT + REQUIRED_CONTEXT
-    run_device_viewer_pluggable(args, plugins=plugins, contexts=contexts, 
+    run_device_viewer_pluggable(plugins=plugins, contexts=contexts,
                                 application=FRONTEND_APPLICATION, persist=False)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
