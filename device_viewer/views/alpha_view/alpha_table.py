a2039a314080e0c0a9e0d8659c63abe15de03041
Vigne-hub <vignesh.rajesh@outlook.com>
2025-09-17 15:10:33 -0400

    display alpha setting view as a group with borders and a group title.

--- device_viewer/views/alpha_view/alpha_table.py
+++ device_viewer/views/alpha_view/alpha_table.py
@@ -1,17 +1,25 @@
-from traitsui.api import View, Item, ObjectColumn, TableEditor, NumericColumn
+from traitsui.api import View, Item, ObjectColumn, TableEditor, NumericColumn, Group
 from device_viewer.views.route_selection_view.route_selection_view import VisibleColumn
 
-alpha_table_view = View(
-    Item('alpha_map', show_label=False, editor=
+alpha_table_editor = TableEditor(
 
-    TableEditor(
+    columns=[
+        ObjectColumn(name='key', label='Value', editable=False,),
+        NumericColumn(name='alpha', label='Alpha'),
+        VisibleColumn(name='visible', label='Visible', editable=False),
 
-        columns=[
-            ObjectColumn(name='key', label='Value', editable=False,),
-            NumericColumn(name='alpha', label='Alpha'),
-            VisibleColumn(name='visible', label='Visible', editable=False),
-        ],
+    ],
 
-        show_column_labels=False,
-    ))
+    show_column_labels=False,
 )
+
+
+alpha_table_view = View(
+    Group(
+        Item('alpha_map', editor=alpha_table_editor, show_label=False),
+        label='Alpha Settings',
+        show_border=True,
+    ),
+)
+
+
