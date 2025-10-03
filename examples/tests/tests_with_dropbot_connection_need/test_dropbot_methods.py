cde3f7e47141db7937e7fe98f83ca40b7765179e
Vigne-hub <vignesh.rajesh@outlook.com>
2025-10-03 17:58:11 -0400

    test dropbot threshold actuation function.

--- examples/tests/tests_with_dropbot_connection_need/test_dropbot_methods.py
+++ examples/tests/tests_with_dropbot_connection_need/test_dropbot_methods.py
@@ -8,8 +8,12 @@ Notes:
 """
 
 import time
+
 from dropbot import EVENT_SHORTS_DETECTED, EVENT_ENABLE, EVENT_CHANNELS_UPDATED
+from dropbot.threshold import actuate_channels
 import numpy as np
+
+from microdrop_utils.hardware_device_monitoring_helpers import check_devices_available
 from ..common import TESTING_BOARD_ELECTRODE_CAPACTIANCE_MIN, proxy_context
 import pytest
 
@@ -21,7 +25,8 @@ def proxy():
     The scope is 'module' to ensure that the same proxy is used across tests,
     and the hardware is accessed sequentially.
     """
-    with proxy_context(ignore=True) as proxy:
+    hwids = ['VID:PID=16C0:0483']
+    with proxy_context(port=check_devices_available(hwids), ignore=True) as proxy:
         yield proxy
 
 
@@ -50,7 +55,7 @@ def test_dropbot_capacitance_updated(proxy):
 
 def test_actuations(proxy):
     """
-    Simple actuation test. See dropbot/threshold.py for a more failsafe method in the function actuate_channels.
+    Simple actuation test. See microdrop.dropbot-plugin/execute.py and dropbot/threshold.py for a more failsafe way.
     """
     n_channels = 10
 
@@ -62,6 +67,7 @@ def test_actuations(proxy):
 
     ########## test triggering actuations ######################
     proxy.state_of_channels = channels
+
     time.sleep(0.5)
     capacitance_after_actuation = proxy.measure_capacitance()
 
@@ -79,6 +85,41 @@ def test_actuations(proxy):
     assert capacitance_after_un_actuation < capacitance_after_actuation
     capacitances = [proxy.measure_capacitance() for _ in range(10)]
     assert (sum(capacitances) / len(capacitances)) < (n_channels * TESTING_BOARD_ELECTRODE_CAPACTIANCE_MIN)
+
+
+def test_actuation_using_dropbot_threshold_method(proxy):
+    """
+    Actuation test. Uses dropbot/threshold.py.
+    """
+    n_channels = 10
+
+    channel_count = proxy.number_of_channels
+    random_channels = np.random.choice(channel_count, n_channels, replace=False).tolist()
+    capacitance_before_actuation = proxy.measure_capacitance()
+
+    ########## test triggering actuations ######################
+
+    # microdrop.dropbot-plugin/execute.py uses timeout of 5
+    actuated_channels = actuate_channels(proxy, random_channels, timeout=5, allow_disabled=True)
+    assert actuated_channels.sort() == random_channels.sort()
+
+    capacitance_after_actuation = proxy.measure_capacitance()
+    assert capacitance_after_actuation > capacitance_before_actuation + TESTING_BOARD_ELECTRODE_CAPACTIANCE_MIN
+
+    capacitances = [proxy.measure_capacitance() for _ in range(10)]
+    assert (sum(capacitances) / len(capacitances)) > (n_channels * TESTING_BOARD_ELECTRODE_CAPACTIANCE_MIN)
+
+    ########## test triggering un-actuations ###################
+
+    actuated_channels = actuate_channels(proxy, [], timeout=5, allow_disabled=True)
+    assert actuated_channels == []
+
+    capacitance_after_un_actuation = proxy.measure_capacitance()
+    assert capacitance_after_un_actuation < capacitance_after_actuation
+
+    capacitances = [proxy.measure_capacitance() for _ in range(10)]
+    assert (sum(capacitances) / len(capacitances)) < (n_channels * TESTING_BOARD_ELECTRODE_CAPACTIANCE_MIN)
+
 
 
 def test_shorts_detected_signal(proxy):
@@ -108,7 +149,7 @@ def test_dropbot_halted(proxy):
     proxy.update_state(capacitance_update_interval_ms=1000,
                        hv_output_selected=True,
                        hv_output_enabled=True,
-                       voltage=75,
+                       voltage=100,
                        event_mask=EVENT_CHANNELS_UPDATED |
                                   EVENT_SHORTS_DETECTED |
                                   EVENT_ENABLE)
@@ -122,7 +163,7 @@ def test_dropbot_halted(proxy):
 
     # Actuate all channels to saturate chip load
     proxy.state_of_channels = np.ones(proxy.number_of_channels)
-    time.sleep(1)
+    time.sleep(5)
 
     assert event["event"] == "halted"
     assert event['error']['name'] == "chip-load-saturated"
