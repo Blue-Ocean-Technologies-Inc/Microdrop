7c971635f7980f5ee9cc5623d7e0061c20eb1474
Vigneshwar Rajesh <67299496+Vigne-hub@users.noreply.github.com>
2025-10-06 23:06:09 -0400

    trimmed quantities from pint formatted to html.

--- microdrop_utils/ureg_helpers.py
+++ microdrop_utils/ureg_helpers.py
@@ -2,21 +2,24 @@ from pint import UnitRegistry
 
 ureg = UnitRegistry()
 
+
 def ureg_quant_percent_change(old, new):
     old = get_ureg_magnitude(old)
     new = get_ureg_magnitude(new)
 
     return 100 * abs(old - new) / old
 
+
 def ureg_diff(old, new):
     old = get_ureg_magnitude(old)
     new = get_ureg_magnitude(new)
 
-
     return old - new
+
 
 def get_ureg_magnitude(text):
     return ureg(text).magnitude
 
+
 def trim_to_n_digits(text, n_digits):
-    return f'{ureg.Quantity(text):.{n_digits}g~}'
+    return f'{ureg.Quantity(text):.#{n_digits}g~H}'
