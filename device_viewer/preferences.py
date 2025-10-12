4f10bf9ad2d1ccc57c2dc792530121e17394b293
Vigneshwar Rajesh <67299496+Vigne-hub@users.noreply.github.com>
2025-10-12 14:34:55 -0400

    Device Loading Into Repo (#76)
    Resolve #73
    
    * change directory names for MicroDrop project folder.
    
    Add preferences category to have preferencs tabs.
    
    cleanup some ids, and path names Use pathlib more.
    
    * Remove uses of app data dir. use the pre existing user data dir for this.
    
    Use a preferences hgroup contructor helper.
    
    * device viewer preferences added.
    
    * Basic Preferences Feat:
    
    - use preferences everywhere for device viewer, what used to be from consts file. Now also from consts file but only on default. New user input via preferences pane is written to file: acts like a changelog.
    
    - Give a consistent nice View for the preferences pane for the various groups.
    
    - New Preferences UI helper functions for making the preference panes.
    
    * Cleanup logs:
    
    Show mostly no info/debug. Exception is dropbot controller. So we get info on proxy status.
    
    (cherry picked from commit c66b436)
    
    * Code clarity:
    
    - Move pref text style to styles module.
    
    - Better naming for func.
    
    - Improve comments.
    
    * example of resetting files to default from a master copy.
    
    * example for repo_file_loading.
    
    * Setup private and public Sci-bots microdrop directories.
    
    private stores ini config files to prevent no UI user modificaitons.
    
    public exposes repos (device repo for example)
    
    Now we copy over master svg file to the device repo.
    
    * We can now load svg files into a device repo.
    
    * Call device repo folder, just Devices.
    Cleanup comments.
    
    * Save file as svg not text.

---
+++ device_viewer/preferences.py
@@ -0,0 +1,148 @@
+from pathlib import Path
+import filecmp
+
+from apptools.preferences.api import PreferencesHelper
+from traits.api import Int, File, Range, Directory
+from traits.etsconfig.api import ETSConfig
+from traitsui.api import VGroup, View, spring, Item, FileEditor
+from envisage.ui.tasks.api import PreferencesCategory
+
+# Enthought library imports.
+from envisage.ui.tasks.api import PreferencesPane
+
+from microdrop_utils.preferences_UI_helpers import create_grid_group, create_item_label_group
+from microdrop_utils.file_handler import safe_copy_file
+from microdrop_utils._logger import get_logger
+
+logger = get_logger(__name__)
+
+from microdrop_style.text_styles import preferences_group_style_sheet
+
+from .consts import DEVICE_VIEWER_SIDEBAR_WIDTH, ALPHA_VIEW_MIN_HEIGHT, LAYERS_VIEW_MIN_HEIGHT, MASTER_SVG_FILE
+
+
+class DeviceViewerPreferences(PreferencesHelper):
+    """The preferences helper, inspired by envisage one for the Attractors application.
+    The underlying preference object is the global default since we do not pass a
+    Preference object. See source code for PreferencesHelper for more details."""
+
+    #### 'PreferencesHelper' interface ########################################
+
+    # The path to the preference node that contains the preferences.
+    preferences_path = "microdrop.device_viewer"
+
+    #### Preferences ##########################################################
+    DEVICE_VIEWER_SIDEBAR_WIDTH = Range(value=DEVICE_VIEWER_SIDEBAR_WIDTH, low=0, high=10000)
+    ALPHA_VIEW_MIN_HEIGHT = Range(value=ALPHA_VIEW_MIN_HEIGHT, low=0, high=10000)
+    LAYERS_VIEW_MIN_HEIGHT = Range(value=LAYERS_VIEW_MIN_HEIGHT, low=0, high=10000)
+
+    DEFAULT_SVG_FILE = File
+
+    DEVICE_REPO_DIR = Directory()
+
+    def _DEVICE_REPO_DIR_default(self) -> Path:
+        default_dir = Path(ETSConfig.user_data) / "Devices"
+
+        default_dir.mkdir(parents=True, exist_ok=True)
+
+        logger.info(f"Default repo directory is: {default_dir}")
+
+        return default_dir
+
+    def _DEFAULT_SVG_FILE_default(self):
+        # --- Define Master File Path (local to the script) ---
+        logger.debug(f"Master svg file is located at: {MASTER_SVG_FILE}")
+
+        if not MASTER_SVG_FILE.exists():
+            logger.error("Master file not found!.")
+            raise FileNotFoundError("Master file not found!.")
+
+        # --- Ensure User's File is a Copy of Master on First Run ---
+        default_user_file = Path(self.DEVICE_REPO_DIR) / MASTER_SVG_FILE.name
+        logger.debug(f"Checking for user's default file: {default_user_file}")
+
+        should_overwrite = True
+
+        if default_user_file.exists():
+            # If the user's file exists, check if it's different from master
+
+            if filecmp.cmp(MASTER_SVG_FILE, default_user_file, shallow=False):
+                logger.info("User's file already exists and matches master.")
+                should_overwrite = False
+
+            else:
+                logger.info("User's default svg file exists but is different from master. Overwriting...")
+
+        else:
+            logger.info("User's default svg file not found, creating it from master...")
+
+        if should_overwrite:
+            default_user_file = safe_copy_file(str(MASTER_SVG_FILE), str(default_user_file))
+
+        return str(default_user_file)
+
+
+device_viewer_tab = PreferencesCategory(
+    id="microdrop.device_viewer.preferences",
+    name="Device Viewer",
+)
+
+
+class DeviceViewerPreferencesPane(PreferencesPane):
+    """Device Viewer preferences pane based on enthought envisage's The preferences pane for the Attractors application."""
+
+    #### 'PreferencesPane' interface ##########################################
+
+    # The factory to use for creating the preferences model object.
+    model_factory = DeviceViewerPreferences
+
+    category = device_viewer_tab.id
+
+    #### 'DeviceViewerPreferencesPane' interface ################################
+
+    # This is the list of trait names for the grid layout
+    sidebar_setting_items = [
+        'DEVICE_VIEWER_SIDEBAR_WIDTH',
+        'ALPHA_VIEW_MIN_HEIGHT',
+        'LAYERS_VIEW_MIN_HEIGHT',
+    ]
+
+    # Create the grid group for the sidebar items.
+    sidebar_settings_grid = create_grid_group(
+        sidebar_setting_items,
+        group_label="Sidebar View",
+        group_show_border=True,  # Example of passing a group kwarg
+        group_style_sheet=preferences_group_style_sheet,
+    )
+
+    # Create the single item for the default svg for the main view group.
+    default_svg_setting_group = create_item_label_group(
+            'DEFAULT_SVG_FILE',
+            label_text='Default Device Layout',
+            item_editor=FileEditor(
+                filter=['SVG Files (*.svg)|*.svg|All Files (*.*)|*.*'],
+                dialog_style='open'
+            ),
+        )
+
+    main_view_settings = VGroup(
+        default_svg_setting_group,
+        label="Main View",
+        show_border=True,
+        style_sheet=preferences_group_style_sheet,
+    ),
+
+    view = View(
+
+        Item("_"),  # Separator
+
+        main_view_settings,
+
+        Item("_"),  # Separator
+
+        sidebar_settings_grid,
+
+        Item("_"),  # Separator to space this out from further contributions to the pane.
+
+        resizable=True
+    )
