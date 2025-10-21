660e4936b965abfa1d020595b50ad9e383e73448
Vigne-hub <vignesh.rajesh@outlook.com>
2025-10-21 10:02:37 -0400

    explicitly state used frontend/backend plugins and contexts, in examples script.

--- examples/run_device_viewer_pluggable_frontend.py
+++ examples/run_device_viewer_pluggable_frontend.py
@@ -1,20 +1,28 @@
 import sys
 import os
 
-from microdrop_utils.broker_server_helpers import dramatiq_workers_context
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from examples.run_device_viewer_pluggable import main as run_device_viewer_pluggable
-from examples.plugin_consts import (REQUIRED_PLUGINS, FRONTEND_PLUGINS,
-                                    FRONTEND_CONTEXT, REQUIRED_CONTEXT,
-                                    FRONTEND_APPLICATION)
+from examples.plugin_consts import *
 
 from logger.plugin import LoggerPlugin
+from dropbot_preferences_ui.plugin import DropbotPreferencesPlugin
+
+FRONTEND_PLUGINS = [
+    TasksPlugin,
+    MicrodropPlugin,
+    # DropbotStatusPlotPlugin,
+    DropbotToolsMenuPlugin,
+    DropbotStatusPlugin,
+    ManualControlsPlugin,
+    ProtocolGridControllerUIPlugin,
+    DeviceViewerPlugin
+]
 
 def main():
     """Run only the frontend plugins."""
-    plugins = REQUIRED_PLUGINS + FRONTEND_PLUGINS + [LoggerPlugin]
+    plugins = REQUIRED_PLUGINS + FRONTEND_PLUGINS + [LoggerPlugin, DropbotPreferencesPlugin]
     contexts = [dramatiq_workers_context]
     run_device_viewer_pluggable(plugins=plugins, contexts=contexts,
                                 application=FRONTEND_APPLICATION, persist=False)
