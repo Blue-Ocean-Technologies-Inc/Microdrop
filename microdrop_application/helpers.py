5f66149dc2e84f613e268d73d5e0ae5eb4750e6f
Vigne-hub <vignesh.rajesh@outlook.com>
2025-10-21 10:02:58 -0400

    sync redis globals helper function for microdrop application.

---
+++ microdrop_application/helpers.py
@@ -0,0 +1,21 @@
+from dramatiq import get_broker
+
+from microdrop_application.consts import APP_GLOBALS_REDIS_HASH
+from microdrop_utils.redis_manager import get_redis_hash_proxy
+
+
+def sync_redis_globals(key, set_value):
+    # try to get experiment directory from app globals
+    app_globals = get_redis_hash_proxy(redis_client=get_broker().client, hash_name=APP_GLOBALS_REDIS_HASH)
+
+    globals_value = app_globals.get(key)
+
+    if globals_value:
+        return globals_value
+
+    elif set_value:
+        # push value to globals if its not there and new value given
+        app_globals[key] = set_value
+        return set_value
+
+    return None
