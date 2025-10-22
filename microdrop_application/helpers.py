1f028ca1464d1b3247f1c475f7430d039fd3cb5f
Vigne-hub <vignesh.rajesh@outlook.com>
2025-10-22 15:53:32 -0400

    Minor: Add docstring.

--- microdrop_application/helpers.py
+++ microdrop_application/helpers.py
@@ -4,8 +4,11 @@ from microdrop_application.consts import APP_GLOBALS_REDIS_HASH
 from microdrop_utils.redis_manager import get_redis_hash_proxy
 
 
-def sync_redis_globals(key, set_value):
-    # try to get experiment directory from app globals
+def sync_redis_globals(key, set_value=None):
+    """
+    returns value already in cache or sets key to set_value if provided.
+    """
+    # try to get key from app globals
     app_globals = get_redis_hash_proxy(redis_client=get_broker().client, hash_name=APP_GLOBALS_REDIS_HASH)
 
     globals_value = app_globals.get(key)
