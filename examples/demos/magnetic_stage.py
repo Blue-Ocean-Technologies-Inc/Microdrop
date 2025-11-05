a4418963a47d22e28f9f50c350bc947bdc9e6d85
Vigne-hub <vignesh.rajesh@outlook.com>
2025-11-05 16:18:29 -0500

    update magnetic stage example script.

--- examples/demos/magnetic_stage.py
+++ examples/demos/magnetic_stage.py
@@ -18,15 +18,17 @@ time.sleep(settling_time_s)
 print(proxy.config)
 
 down_height = proxy.config.zstage_down_position
+
+print(down_height == 0.5)
+
 up_height = proxy.config.zstage_up_position
+print(up_height)
 
 zstage = proxy.zstage
 
-## ensure zstage is homed
+## ensure zstage is homed at 0.0
 zstage.home()
-print(zstage.position == down_height)
-print(zstage.is_down == True)
-print(zstage.is_up == False)
+print(zstage.position == 0.0)
 time.sleep(0.5)
 
 ## bring stage up
@@ -42,12 +44,8 @@ print(zstage.is_down == True)
 print(zstage.is_up == False)
 
 time.sleep(0.5)
+
 # home again
 zstage.home()
 
 proxy.terminate()
-
-
-
-
-
