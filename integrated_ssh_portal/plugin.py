97897f73c3cceb07fb92495a388d30c81695a0a3
Vigne-hub <vignesh.rajesh@outlook.com>
2025-11-18 17:16:04 -0500

    An integrated ssh portal plugin. And demo file.

---
+++ integrated_ssh_portal/plugin.py
@@ -0,0 +1,43 @@
+# enthought imports
+from pyface.action.schema.schema_addition import SchemaAddition
+from traits.api import List, Str
+from envisage.api import Plugin, TASK_EXTENSIONS
+from envisage.ui.tasks.api import TaskExtension
+
+from microdrop_application.consts import PKG as microdrop_application_PKG
+
+from .consts import PKG, PKG_name
+
+
+class IntegratedSshPortalPlugin(Plugin):
+    #### 'IPlugin' interface ##################################################
+
+    #: The plugin unique identifier.
+    id = PKG + ".plugin"
+
+    #: The plugin name (suitable for displaying to the user).
+    name = f"{PKG_name} Plugin"
+
+    #: The task id to contribute task extension view to
+    task_id_to_contribute_view = Str(default_value=f"{microdrop_application_PKG}.task")
+
+    #### Contributions to extension points made by this plugin ################
+
+    contributed_task_extensions = List(contributes_to=TASK_EXTENSIONS)
+    #### Trait initializers ###################################################
+
+    def _contributed_task_extensions_default(self):
+        from .menus import menu_factory
+
+        return [
+            TaskExtension(
+                task_id=self.task_id_to_contribute_view,
+                actions=[
+                    SchemaAddition(
+                        factory=menu_factory,
+                        path='MenuBar/Edit',
+                    )
+
+                ]
+            )
+        ]
