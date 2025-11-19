a0c4a62253de931bd0508e84a1cc92a09e6a05a4
Vigne-hub <vignesh.rajesh@outlook.com>
2025-11-19 16:53:47 -0500

    Create a range class with more view hints for a custom logic.

--- peripheral_controller/preferences.py
+++ peripheral_controller/preferences.py
@@ -1,5 +1,7 @@
 from apptools.preferences.api import PreferencesHelper
-from traits.api import Float, Int, Dict, Property, Range
+from traits.api import Float, Int, Dict, Property, Range, observe
+from pyface.api import warning
+
 from logger.logger_service import get_logger
 
 from .consts import DEFAULT_UP_HEIGHT_MM, DEFAULT_DOWN_HEIGHT_MM, MAX_ZSTAGE_HEIGHT_MM, MIN_ZSTAGE_HEIGHT_MM
@@ -13,6 +15,32 @@ z_stage_preferences_names = [
 
 app_globals = get_microdrop_redis_globals_manager()
 
+class RangeWithViewHints(Range):
+    def create_editor(self):
+        """ Returns the default UI editor for the trait.
+        """
+        # fixme: Needs to support a dynamic range editor.
+
+        auto_set = self.auto_set
+        if auto_set is None:
+            auto_set = True
+
+        from traitsui.api import RangeEditor
+
+        return RangeEditor(
+            self,
+            mode=self.mode or "auto",
+            cols=self.cols or 3,
+            auto_set=auto_set,
+            enter_set=self.enter_set or False,
+            low_label=self.low or "",
+            high_label=self.high or "",
+            low_name=self._low_name,
+            high_name=self._high_name,
+            format_str='%.2f',
+            is_float=True
+        )
+
 class PeripheralPreferences(PreferencesHelper):
     """The preferences helper, inspired by envisage one for the Attractors application.
     The underlying preference object is the global default since we do not pass a
@@ -24,17 +52,31 @@ class PeripheralPreferences(PreferencesHelper):
     preferences_path = "microdrop.peripheral_settings"
 
 
-    up_height_mm = Range(value=DEFAULT_UP_HEIGHT_MM, low=MIN_ZSTAGE_HEIGHT_MM, high=MAX_ZSTAGE_HEIGHT_MM,
-                         desc="Height of stage when up command sent")
+    up_height_mm = RangeWithViewHints(
+        value=DEFAULT_UP_HEIGHT_MM,
+        low=MIN_ZSTAGE_HEIGHT_MM + 0.1,
+        high=MAX_ZSTAGE_HEIGHT_MM,
+        desc="Height of stage when up command sent"
+    )
 
-    down_height_mm = Range(value=DEFAULT_DOWN_HEIGHT_MM, low=MIN_ZSTAGE_HEIGHT_MM, high=DEFAULT_UP_HEIGHT_MM,
-                         desc="Height of stage when down command sent")
+    _max_down_height = Property(observe="up_height_mm")
+
+    down_height_mm = RangeWithViewHints(
+        value=DEFAULT_DOWN_HEIGHT_MM,
+        low=MIN_ZSTAGE_HEIGHT_MM,
+        high="_max_down_height",
+        desc="Height of stage when down command sent"
+    )
 
     #### Preferences ##########################################################
 
     preferences_name_map = Property(Dict)
 
     ################ View Model ################################################
+
+    def _get__max_down_height(self):
+        return self.up_height_mm - 0.1
+
 
     def _get_preferences_name_map(self):
         # Use a dict comprehension to build the dictionary
