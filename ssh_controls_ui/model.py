b2f928702da474125be0621f4396452a63e7c3c6
Vigne-hub <vignesh.rajesh@outlook.com>
2025-11-19 11:07:56 -0500

    fix type for port: should be int. And load the dotenv here.

--- ssh_controls_ui/model.py
+++ ssh_controls_ui/model.py
@@ -1,14 +1,17 @@
 import os
 
-from traits.api import HasTraits, Str
+from traits.api import HasTraits, Str, Int
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class SSHControlModel(HasTraits):
     """A dataclass to hold the application's state."""
     host = Str(os.getenv("REMOTE_HOST_IP_ADDRESS"))
-    port = Str(os.getenv("REMOTE_SSH_PORT"))
+    port = Int(os.getenv("REMOTE_SSH_PORT"))
     username = Str(os.getenv("REMOTE_HOST_USERNAME"))
     password = Str(os.getenv("REMOTE_PASSWORD"))
     key_name = "id_rsa_microdrop"
-    generated_pub_key = ""
-    generated_pub_key_path: str = ""
+    generated_pub_key = Str
+    generated_pub_key_path = Str
