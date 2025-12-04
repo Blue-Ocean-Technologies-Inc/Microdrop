bc515da75dcb1ceac6dd24bf560b6aa736f6f127
Vigne-hub <vignesh.rajesh@outlook.com>
2025-12-04 14:52:03 -0500

    pytests for new dmf utils methods.

--- examples/tests/test_dmf_utils.py
+++ examples/tests/test_dmf_utils.py
@@ -10,8 +10,8 @@ from xml.etree import ElementTree as ET
 def clean_svg():
     from .common import TEST_PATH
     with tempfile.TemporaryDirectory() as tmpdir:
-        shutil.copy(f"{TEST_PATH}{os.sep}device_svg_files{os.sep}2x3device.svg", tmpdir)
-        yield Path(tmpdir) / '2x3device.svg'
+        shutil.copy(f"{TEST_PATH}{os.sep}device_svg_files{os.sep}90_pin_array.svg", tmpdir)
+        yield Path(tmpdir) / '90_pin_array.svg'
 
 
 @pytest.fixture
@@ -34,6 +34,11 @@ def SvgUtil():
     from device_viewer.utils.dmf_utils import SvgUtil
     return SvgUtil
 
+@pytest.fixture
+def SVGProcessor():
+    from device_viewer.utils.dmf_utils_helpers import SVGProcessor
+    return SVGProcessor
+
 def test_svg_util(clean_svg, SvgUtil):
 
     SvgUtil(clean_svg)
@@ -52,7 +57,21 @@ def test_set_fill_black(svg_electrode_layer, SvgUtil):
     assert svg_electrode_layer[0].attrib['style'] == 'fill:#000000'
 
 
-def test_svg_to_electrodes(svg_electrode_layer, SvgUtil):
-    svg = SvgUtil()
-    assert len(svg.svg_to_electrodes(svg_electrode_layer)) == 92
+def test_svg_to_electrodes(clean_svg, SVGProcessor):
+    svg_processor = SVGProcessor(filename=str(clean_svg))
+    for child in svg_processor.root:
+        if "Device" in child.attrib.values():
+            electrodes = svg_processor.svg_to_electrodes(child)
+            assert len(electrodes) == 84
 
+def test_extract_connections(clean_svg, SVGProcessor):
+    svg_processor = SVGProcessor(filename=str(clean_svg))
+    for child in svg_processor.root:
+        if "Connections" in child.attrib.values():
+            connection_lines = svg_processor.extract_connections(child)
+
+            assert len(connection_lines) == 125
+
+def test_get_connection_lines(clean_svg, SvgUtil):
+    svg = SvgUtil(clean_svg)
+    assert (len(svg.get_connection_lines())) == 125
