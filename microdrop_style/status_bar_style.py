1fbf4a32c3a76c769b9aa67a3f69d126d8d1ed08
Vigne-hub <vignesh.rajesh@outlook.com>
2025-12-15 19:27:22 -0500

    optimize imports / syntax

--- microdrop_style/status_bar_style.py
+++ microdrop_style/status_bar_style.py
@@ -1,17 +1,16 @@
-from microdrop_style.helpers import is_dark_mode
-
 DARK_MODE_STYLESHEET = """
-QStatusBar {
-    color: #dadedf;              
-    font-weight: bold;  
-    font-size: 14x; 
-    font-family: Arial;
-    background: #222222;
-    border-top: 2px solid #333333 ;
-    border-bottom: 2px solid #333333;
-}
-QStatusBar::item {border: None;}
-"""
+            QStatusBar {
+                color: #dadedf;              
+                font-weight: bold;  
+                font-size: 14x; 
+                font-family: Arial;
+                background: #222222;
+                border-top: 2px solid #333333 ;
+                border-bottom: 2px solid #333333;
+            }
+            QStatusBar::item {border: None;}
+            
+            """
 
 LIGHT_MODE_STYLESHEET = """
             QStatusBar {
@@ -24,7 +23,7 @@ LIGHT_MODE_STYLESHEET = """
                 border-bottom: 2px solid #dadedf;
             }
             QStatusBar::item {border: None;}
-         """
+            """
 
 def get_status_bar_stylesheet(theme):
     if theme == 'dark':
