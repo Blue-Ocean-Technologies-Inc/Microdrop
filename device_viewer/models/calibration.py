7119ac5ffe154e5c343369870d02dda163640baa
Vigne-hub <vignesh.rajesh@outlook.com>
2026-01-04 21:13:21 -0500

    reset calibration properly to None.

--- device_viewer/models/calibration.py
+++ device_viewer/models/calibration.py
@@ -1,15 +1,18 @@
 from traits.api import HasTraits, Float, Instance
 
 from device_viewer.models.electrodes import Electrodes
-from device_viewer.models.main_model import logger
+
+from logger.logger_service import get_logger
+
+logger = get_logger(__name__)
 
 
 class CalibrationModel(HasTraits):
 
     last_capacitance = Float(desc="last capacitance reported by dropbot in pF")  # pF
 
-    liquid_capacitance_over_area = Float(allow_none=True, desc="Measure capacitance in selected electrodes covered by liquid normalized over electrode area in pF/mm^2")
-    filler_capacitance_over_area = Float(allow_none=True, desc="Measure capacitance in selected electrodes covered by air normalized over electrode area in pF/mm^2")
+    liquid_capacitance_over_area = Float(allow_none=True, default_value=None, desc="Measure capacitance in selected electrodes covered by liquid normalized over electrode area in pF/mm^2")
+    filler_capacitance_over_area = Float(allow_none=True, default_value=None, desc="Measure capacitance in selected electrodes covered by air normalized over electrode area in pF/mm^2")
 
     # Reference to electrodes for area calculations
     electrodes = Instance(Electrodes)
@@ -33,5 +36,4 @@ class CalibrationModel(HasTraits):
             self.liquid_capacitance_over_area = self.last_capacitance / area
 
     def reset(self):
-        self.liquid_capacitance_over_area = None
-        self.filler_capacitance_over_area = None
+        self.reset_traits(["filler_capacitance_over_area", "liquid_capacitance_over_area"])
