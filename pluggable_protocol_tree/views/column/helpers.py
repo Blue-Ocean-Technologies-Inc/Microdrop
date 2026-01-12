96f17f6232779b508752bb6abca5976610e5136b
Vigne-hub <vignesh.rajesh@outlook.com>
2026-01-12 16:56:20 -0500

    consolidate columnn creator helpers. Allow setting a handler from the function call.

--- pluggable_protocol_tree/views/column/helpers.py
+++ pluggable_protocol_tree/views/column/helpers.py
@@ -14,33 +14,50 @@ from ...models.column import (
     BaseColumnModel,
 )
 
+from typing import Any, Type
 
-def get_double_spinner_column(id, name, low, high, decimals, single_step):
+def _create_column(
+    col_id: str,
+    col_name: str,
+    model_cls: Type,
+    view_cls: Type,
+    handler: Any = None,
+    **model_kwargs
+):
+    """
+    Internal factory to consolidate Column instantiation logic.
+    """
     return Column(
-        model=BaseDoubleSpinBoxColumnModel(
-            col_id=id, col_name=name, low=low, high=high, decimals=decimals, single_step=single_step
-        ),
-        view=DoubleSpinBoxColumnView(),
+        model=model_cls(col_id=col_id, col_name=col_name, **model_kwargs),
+        view=view_cls(),
+        handler=handler,
     )
 
+# --- Public API ---
 
-def get_int_spinner_column(id, name, low, high):
-    return Column(
-        model=BaseIntSpinBoxColumnModel(col_id=id, col_name=name, low=low, high=high),
-        view=IntSpinBoxColumnView(),
+def get_double_spinner_column(id, name, low, high, decimals=2, single_step=0.1, handler=None):
+    return _create_column(
+        id, name,
+        BaseDoubleSpinBoxColumnModel, DoubleSpinBoxColumnView, handler,
+        low=low, high=high, decimals=decimals, single_step=single_step
     )
 
-
-def get_checkbox_column(id, name):
-    return Column(
-        model=BaseColumnModel(col_id=id, col_name=name),
-        view=CheckboxView(),
-        handler=CheckboxHandler(),
+def get_int_spinner_column(id, name, low, high, handler=None):
+    return _create_column(
+        id, name,
+        BaseIntSpinBoxColumnModel, IntSpinBoxColumnView, handler,
+        low=low, high=high
     )
 
+def get_checkbox_column(id, name, handler=None):
+    # Consolidate the specific default handler logic here
+    return _create_column(
+        id, name,
+        BaseColumnModel, CheckboxView, handler or CheckboxHandler()
+    )
 
-def get_string_editor_column(id, name):
-    return Column(
-        model=BaseColumnModel(col_id=id, col_name=name),
-        view=StringEditColumnView(),
+def get_string_editor_column(id, name, handler=None):
+    return _create_column(
+        id, name,
+        BaseColumnModel, StringEditColumnView, handler
     )
