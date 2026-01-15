a78d2122f2c0421cec53f31b35e0bae0c0af82f7
Vigne-hub <vignesh.rajesh@outlook.com>
2026-01-15 14:23:10 -0500

    Separating device view preferences view contents from the pane class. Now they can be imported elsewhere.

--- device_viewer/preferences.py
+++ device_viewer/preferences.py
@@ -1,25 +1,33 @@
-from pathlib import Path
 import filecmp
-
-from apptools.preferences.api import PreferencesHelper
-from traits.api import File, Range, Directory, Dict, Property
-from traits.etsconfig.api import ETSConfig
-from traitsui.api import VGroup, View, Item, FileEditor, Group
-from envisage.ui.tasks.api import PreferencesCategory
+from pathlib import Path
 
 # Enthought library imports.
-from envisage.ui.tasks.api import PreferencesPane
+from apptools.preferences.api import PreferencesHelper
+from envisage.ui.tasks.api import PreferencesCategory, PreferencesPane
+from traits.api import Dict, Directory, File, Property, Range
+from traits.etsconfig.api import ETSConfig
+from traitsui.api import FileEditor, Group, Item, View
 
-from microdrop_utils.preferences_UI_helpers import create_grid_group, create_item_label_group, create_item_label_pair
-from microdrop_utils.file_handler import safe_copy_file
 from logger.logger_service import get_logger
+from microdrop_utils.file_handler import safe_copy_file
+from microdrop_utils.preferences_UI_helpers import (
+    create_grid_group,
+    create_item_label_group,
+    create_item_label_pair,
+)
 
 logger = get_logger(__name__)
 
 from microdrop_style.text_styles import preferences_group_style_sheet
 
-from .consts import DEVICE_VIEWER_SIDEBAR_WIDTH, ALPHA_VIEW_MIN_HEIGHT, LAYERS_VIEW_MIN_HEIGHT, MASTER_SVG_FILE, \
-    AUTO_FIT_MARGIN_SCALE, ZOOM_SENSITIVITY
+from .consts import (
+    ALPHA_VIEW_MIN_HEIGHT,
+    AUTO_FIT_MARGIN_SCALE,
+    DEVICE_VIEWER_SIDEBAR_WIDTH,
+    LAYERS_VIEW_MIN_HEIGHT,
+    MASTER_SVG_FILE,
+    ZOOM_SENSITIVITY,
+)
 from .default_settings import default_alphas, default_visibility
 
 
@@ -35,7 +43,9 @@ class DeviceViewerPreferences(PreferencesHelper):
 
     #### Preferences ##########################################################
     ### Side bar prefs ###
-    DEVICE_VIEWER_SIDEBAR_WIDTH = Range(value=DEVICE_VIEWER_SIDEBAR_WIDTH, low=0, high=10000)
+    DEVICE_VIEWER_SIDEBAR_WIDTH = Range(
+        value=DEVICE_VIEWER_SIDEBAR_WIDTH, low=0, high=10000
+    )
     ALPHA_VIEW_MIN_HEIGHT = Range(value=ALPHA_VIEW_MIN_HEIGHT, low=0, high=10000)
     LAYERS_VIEW_MIN_HEIGHT = Range(value=LAYERS_VIEW_MIN_HEIGHT, low=0, high=10000)
 
@@ -43,12 +53,14 @@ class DeviceViewerPreferences(PreferencesHelper):
     default_alphas = Dict(default_alphas)
 
     ### main view prefs ###
-    AUTO_FIT_MARGIN_SCALE = Range(value=AUTO_FIT_MARGIN_SCALE, low=1, high=100, mode="spinner")
-    ZOOM_SENSITIVITY = Range(value=ZOOM_SENSITIVITY, low=1, high=100, mode='spinner')
+    AUTO_FIT_MARGIN_SCALE = Range(
+        value=AUTO_FIT_MARGIN_SCALE, low=1, high=100, mode="spinner"
+    )
+    ZOOM_SENSITIVITY = Range(value=ZOOM_SENSITIVITY, low=1, high=100, mode="spinner")
 
     # getters for processed values from int set in spinner
-    _auto_fit_margin_scale = Property(observe='AUTO_FIT_MARGIN_SCALE')
-    _zoom_scale = Property(observe='ZOOM_SENSITIVITY')
+    _auto_fit_margin_scale = Property(observe="AUTO_FIT_MARGIN_SCALE")
+    _zoom_scale = Property(observe="ZOOM_SENSITIVITY")
 
     DEFAULT_SVG_FILE = File
 
@@ -85,13 +97,17 @@ class DeviceViewerPreferences(PreferencesHelper):
                 should_overwrite = False
 
             else:
-                logger.info("User's default svg file exists but is different from master. Overwriting...")
+                logger.info(
+                    "User's default svg file exists but is different from master. Overwriting..."
+                )
 
         else:
             logger.info("User's default svg file not found, creating it from master...")
 
         if should_overwrite:
-            default_user_file = safe_copy_file(str(MASTER_SVG_FILE), str(default_user_file))
+            default_user_file = safe_copy_file(
+                str(MASTER_SVG_FILE), str(default_user_file)
+            )
 
         return str(default_user_file)
 
@@ -107,6 +123,55 @@ device_viewer_tab = PreferencesCategory(
     name="Device Viewer",
 )
 
+# Define device viewer preferences pane view contents
+
+# This is the list of trait names for the grid layout
+sidebar_setting_items = [
+    "DEVICE_VIEWER_SIDEBAR_WIDTH",
+    "ALPHA_VIEW_MIN_HEIGHT",
+    "LAYERS_VIEW_MIN_HEIGHT",
+]
+
+# Create the grid group for the sidebar items.
+sidebar_settings_grid = create_grid_group(
+    sidebar_setting_items,
+    group_label="Sidebar View",
+    group_show_border=True,  # Example of passing a group kwarg
+    group_style_sheet=preferences_group_style_sheet,
+)
+
+########## Main view grid ###########################
+# Create items for the default svg for the main view group.
+default_svg_setting_item = create_item_label_pair(
+    "DEFAULT_SVG_FILE",
+    label_text="Default Device Layout",
+    item_editor=FileEditor(
+        filter=["SVG Files (*.svg)|*.svg|All Files (*.*)|*.*"], dialog_style="open"
+    ),
+)
+
+default_auto_fit_margin_scale_item = create_item_label_group(
+    "AUTO_FIT_MARGIN_SCALE",
+)
+
+default_zoom_sensitivity = create_item_label_group(
+    "ZOOM_SENSITIVITY",
+)
+
+main_view_settings = (
+    Group(
+        [
+            default_svg_setting_item,
+            default_auto_fit_margin_scale_item,
+            default_zoom_sensitivity,
+        ],
+        label="Main View",
+        show_labels=False,
+        show_border=True,
+        style_sheet=preferences_group_style_sheet,
+    ),
+)
+
 
 class DeviceViewerPreferencesPane(PreferencesPane):
     """Device Viewer preferences pane based on enthought envisage's The preferences pane for the Attractors application."""
@@ -118,63 +183,15 @@ class DeviceViewerPreferencesPane(PreferencesPane):
 
     category = device_viewer_tab.id
 
-    #### 'DeviceViewerPreferencesPane' interface ################################
-
-    # This is the list of trait names for the grid layout
-    sidebar_setting_items = [
-        'DEVICE_VIEWER_SIDEBAR_WIDTH',
-        'ALPHA_VIEW_MIN_HEIGHT',
-        'LAYERS_VIEW_MIN_HEIGHT',
-    ]
-
-    # Create the grid group for the sidebar items.
-    sidebar_settings_grid = create_grid_group(
-        sidebar_setting_items,
-        group_label="Sidebar View",
-        group_show_border=True,  # Example of passing a group kwarg
-        group_style_sheet=preferences_group_style_sheet,
-    )
-
-    ########## Main view grid ###########################
-    # Create items for the default svg for the main view group.
-    default_svg_setting_item = create_item_label_pair(
-            'DEFAULT_SVG_FILE',
-            label_text='Default Device Layout',
-            item_editor=FileEditor(
-                filter=['SVG Files (*.svg)|*.svg|All Files (*.*)|*.*'],
-                dialog_style='open'
-            ),
-        )
-
-    default_auto_fit_margin_scale_item = create_item_label_group(
-            'AUTO_FIT_MARGIN_SCALE',
-    )
-
-    default_zoom_sensitivity = create_item_label_group(
-        'ZOOM_SENSITIVITY',
-    )
-
-    main_view_settings = Group(
-        [default_svg_setting_item, default_auto_fit_margin_scale_item, default_zoom_sensitivity],
-        label="Main View",
-        show_labels = False,
-        show_border=True,
-        style_sheet=preferences_group_style_sheet,
-    ),
-
     ########################################################################################
 
     view = View(
-
         Item("_"),  # Separator
-
         main_view_settings,
-
         Item("_"),  # Separator
-
         sidebar_settings_grid,
-
-        Item("_"),  # Separator to space this out from further contributions to the pane.
-
-        resizable=True
+        Item(
+            "_"
+        ),  # Separator to space this out from further contributions to the pane.
+        resizable=True,
     )
