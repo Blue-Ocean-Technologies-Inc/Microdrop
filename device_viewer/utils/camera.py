3616898bb6bd57e069cb97eaa1b3213255dc33e5
Vigne-hub <vignesh.rajesh@outlook.com>
2026-01-27 11:20:28 -0500

    [chore] - Remove cv2 requirement.

--- device_viewer/utils/camera.py
+++ device_viewer/utils/camera.py
@@ -1,27 +1,27 @@
 import json
 from typing import List, Tuple
 
-import numpy as np
-import cv2
+# import numpy as np
 from PySide6.QtCore import QPointF
 from PySide6.QtGui import QImage, QTransform
 
-def qimage_to_cv_image(qimage: QImage) -> np.ndarray: # https://stackoverflow.com/questions/18406149/pyqt-pyside-how-do-i-convert-qimage-into-opencvs-mat-format
-    '''  Converts a QImage into an opencv MAT format  '''
-    incomingImage = qimage.convertToFormat(QImage.Format_RGBX8888)
-    ptr = incomingImage.constBits()
-    cv_im_in = np.array(ptr, copy=True).reshape(incomingImage.height(), incomingImage.width(), 4)
-    cv_im_in = cv2.cvtColor(cv_im_in, cv2.COLOR_BGRA2RGB)
-    return cv_im_in
-
-def cv_image_to_qimage(cv_image: np.ndarray) -> QImage:
-    ''' Converts an opencv MAT format image into a QImage '''
-    # Convert BGR (OpenCV) to RGB
-    rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-    h, w, ch = rgb_image.shape
-
-    # Create QImage and wrap in QVideoFrame
-    return QImage(rgb_image.data, w, h, ch * w, QImage.Format_RGB888).copy()  # Use copy to ensure data is not shared with OpenCV
+# import cv2
+# def qimage_to_cv_image(qimage: QImage) -> np.ndarray: # https://stackoverflow.com/questions/18406149/pyqt-pyside-how-do-i-convert-qimage-into-opencvs-mat-format
+#     '''  Converts a QImage into an opencv MAT format  '''
+#     incomingImage = qimage.convertToFormat(QImage.Format_RGBX8888)
+#     ptr = incomingImage.constBits()
+#     cv_im_in = np.array(ptr, copy=True).reshape(incomingImage.height(), incomingImage.width(), 4)
+#     cv_im_in = cv2.cvtColor(cv_im_in, cv2.COLOR_BGRA2RGB)
+#     return cv_im_in
+#
+# def cv_image_to_qimage(cv_image: np.ndarray) -> QImage:
+#     ''' Converts an opencv MAT format image into a QImage '''
+#     # Convert BGR (OpenCV) to RGB
+#     rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+#     h, w, ch = rgb_image.shape
+#
+#     # Create QImage and wrap in QVideoFrame
+#     return QImage(rgb_image.data, w, h, ch * w, QImage.Format_RGB888).copy()  # Use copy to ensure data is not shared with OpenCV
 
 def qtransform_serialize(transform: QTransform) -> str:
     return json.dumps([transform.m11(), transform.m12(), transform.m13(),
