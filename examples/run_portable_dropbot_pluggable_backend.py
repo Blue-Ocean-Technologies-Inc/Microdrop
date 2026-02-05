90e425566f70165f7e3fdfd2a2d0c80588d4f707
Vigne-hub <vignesh.rajesh@outlook.com>
2026-02-05 10:41:32 -0500

    [Port DB Backend] Runner script.

---
+++ examples/run_portable_dropbot_pluggable_backend.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+from portable_dropbot_controller.plugin import PortDropbotControllerPlugin
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from examples.run_device_viewer_pluggable import main as run_device_viewer_pluggable
+from examples.plugin_consts import *
+
+
+BACKEND_PLUGINS = [
+    PortDropbotControllerPlugin
+]
+
+def main():
+    """Run only the backend plugins."""
+
+    plugins = REQUIRED_PLUGINS + BACKEND_PLUGINS
+
+    run_device_viewer_pluggable(plugins=plugins, contexts=REQUIRED_CONTEXT + SERVER_CONTEXT, application=BACKEND_APPLICATION, persist=True)
+
+
+if __name__ == "__main__":
+    main()
