3b98b46bbe45559ba8d17809f0a81538b133a09c
Vigne-hub <vignesh.rajesh@outlook.com>
2026-02-05 19:48:49 -0500

    simpler units handling.

--- portable_dropbot_status/dramatiq_UI.py
+++ portable_dropbot_status/dramatiq_UI.py
@@ -169,8 +169,8 @@ class DramatiqDropBotStatusViewModel(HasTraits):
             return "-"
 
         self.model.frequency = format_val("hv_freq", "Hz")
-        self.model.chip_temp = format_val("cur_temp", "degC")
-        self.model.device_temp = format_val("dev_temp", "°degC")
+        self.model.chip_temp = format_val("cur_temp", "celsius")
+        self.model.device_temp = format_val("dev_temp", "celsius")
         self.model.device_humidity = format_val("dev_hum", "%")
 
     ####### Dropbot Icon Image Control Methods ###########
