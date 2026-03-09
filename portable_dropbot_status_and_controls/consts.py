f9057635a4f75c93e2308a587d3da94361782954
Vigne-hub <vignesh.rajesh@outlook.com>
2026-03-09 12:40:41 -0400

    listener name var

--- portable_dropbot_status_and_controls/consts.py
+++ portable_dropbot_status_and_controls/consts.py
@@ -6,11 +6,15 @@ from dropbot_controller.consts import (
     CHIP_INSERTED,
 )
 from peripheral_controller.consts import ZSTAGE_POSITION_UPDATED
-from portable_dropbot_controller.consts import PORT_DROPBOT_STATUS_UPDATE, TRAY_TOGGLE_FAILED
+from portable_dropbot_controller.consts import (
+    PORT_DROPBOT_STATUS_UPDATE,
+    TRAY_TOGGLE_FAILED,
+)
 
 # # This module's package.
 PKG = ".".join(__name__.split(".")[:-1])
 PKG_name = PKG.title().replace("_", " ")
+listener_name = f"{PKG}_listener"
 
 current_folder_path = os.path.dirname(os.path.abspath(__file__))
 DROPBOT_IMAGE = os.path.join(current_folder_path, "images", "dropbot.png")
@@ -20,7 +24,7 @@ DROPBOT_CHIP_INSERTED_IMAGE = os.path.join(
 
 # Topics actor declared by plugin subscribes to
 ACTOR_TOPIC_DICT = {
-    f"{PKG}_listener": [
+    listener_name: [
         PORT_DROPBOT_STATUS_UPDATE,
         "ui/calibration_data",
         DROPBOT_CONNECTED,
