0891cbbe9651e779af4951be7402c8d987fc5278
Vigne-hub <vignesh.rajesh@outlook.com>
2026-03-18 17:37:13 -0400

    [Feat] user help plugin. Create help menu in toolbar. Populate with send feedback dialog.

---
+++ user_help_plugin/consts.py
@@ -0,0 +1,5 @@
+# This module's package.
+PKG = '.'.join(__name__.split('.')[:-1])
+PKG_name = PKG.title().replace("_", " ")
+
+FEEDBACK_URL = "https://blueoceantechnologies.ca/feedback"
---
+++ user_help_plugin/feedback_dialog.py
@@ -0,0 +1,19 @@
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from pyface.qt.QtWidgets import QDialog, QVBoxLayout
+from pyface.qt.QtCore import QUrl
+
+from .consts import FEEDBACK_URL
+
+
+class SendFeedbackDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Send Feedback")
+        self.resize(600, 700)
+
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.web_view = QWebEngineView()
+        self.web_view.load(QUrl(FEEDBACK_URL))
+        layout.addWidget(self.web_view)
