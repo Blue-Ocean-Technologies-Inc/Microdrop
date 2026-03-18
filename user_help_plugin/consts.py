ab0a663a2be7a7036f8cc234193154d2f731a4b3
Vigne-hub <vignesh.rajesh@outlook.com>
2026-03-18 18:03:25 -0400

    [Feat] add more help options

--- user_help_plugin/consts.py
+++ user_help_plugin/consts.py
@@ -3,3 +3,5 @@ PKG = '.'.join(__name__.split('.')[:-1])
 PKG_name = PKG.title().replace("_", " ")
 
 FEEDBACK_URL = "https://blueoceantechnologies.ca/feedback"
+GITHUB_ISSUES_URL = "https://github.com/Blue-Ocean-Technologies-Inc/Microdrop/issues"
+SCIBOTS_URL = "https://sci-bots.com"
