56b307854c96a13971227cc60be9ecbb4e2f7c42
Vigne-hub <vignesh.rajesh@outlook.com>
2026-04-28 13:53:11 -0400

    [PPT-5] Wire PeripheralProtocolControlsPlugin to contribute magnet column

--- peripheral_protocol_controls/plugin.py
+++ peripheral_protocol_controls/plugin.py
@@ -12,7 +12,13 @@ from traits.api import List, Instance
 
 from logger.logger_service import get_logger
 
+from pluggable_protocol_tree.consts import PROTOCOL_COLUMNS
+from pluggable_protocol_tree.interfaces.i_compound_column import (
+    ICompoundColumn,
+)
+
 from .consts import PKG, PKG_name
+from .protocol_columns.magnet_column import make_magnet_column
 
 
 logger = get_logger(__name__)
@@ -22,4 +28,9 @@ class PeripheralProtocolControlsPlugin(Plugin):
     id = PKG + '.plugin'
     name = f'{PKG_name} Plugin'
 
-    # contributed_protocol_columns is added in task 6.
+    contributed_protocol_columns = List(
+        Instance(ICompoundColumn), contributes_to=PROTOCOL_COLUMNS,
+    )
+
+    def _contributed_protocol_columns_default(self):
+        return [make_magnet_column()]
--- peripheral_protocol_controls/tests/test_plugin_shell.py
+++ peripheral_protocol_controls/tests/test_plugin_shell.py
@@ -17,3 +17,19 @@ def test_plugin_instantiates_with_no_columns_yet():
     p = PeripheralProtocolControlsPlugin()
     assert hasattr(p, "id")
     assert hasattr(p, "name")
+
+
+def test_plugin_contributes_magnet_compound_column():
+    """The plugin's contributed_protocol_columns default factory yields
+    a list containing the magnet CompoundColumn."""
+    from peripheral_protocol_controls.plugin import (
+        PeripheralProtocolControlsPlugin,
+    )
+    from pluggable_protocol_tree.interfaces.i_compound_column import (
+        ICompoundColumn,
+    )
+    p = PeripheralProtocolControlsPlugin()
+    cols = p.contributed_protocol_columns
+    assert len(cols) == 1
+    assert isinstance(cols[0], ICompoundColumn)
+    assert cols[0].model.base_id == "magnet"
